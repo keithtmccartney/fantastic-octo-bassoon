5e262f5ac2d6d7a6231d97a6d360fcac751fe40e
Keith <keithtmccartney@hotmail.com>
2020-10-27 12:38:24 +0000

    Adding a new ASP.NET Core Web Application with MVC.

---
+++ fantastic-octo-bassoon/fantastic-octo-bassoon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fantastic_octo_bassoon.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
